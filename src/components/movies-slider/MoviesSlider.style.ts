48d69da6685c7703c281fe8eacfddb484ffe3b44
S2UZY <pc_suzi@daum.net>
2024-11-11 03:36:44 +0900

    style: add movie-slider style, animation

---
+++ src/components/movies-slider/MoviesSlider.style.ts
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+import { styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+export const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+export const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+export const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
