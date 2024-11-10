c5b79f7cd5ac7f5707c4c0eac81a2db4745df2ad
S2UZY <pc_suzi@daum.net>
2024-11-11 03:38:40 +0900

    feat: add routerProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
