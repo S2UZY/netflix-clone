82408010454744398444f60e42ff7036519920fa
S2UZY <pc_suzi@daum.net>
2024-11-11 03:38:28 +0900

    feat: add queryClientProvider, themeProvider, globalStyle, header, outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import { Header } from "./components";
+import { GlobalStyle, theme } from "./styles";
+
+const client = new QueryClient();
+
 function App() {
-  return <div></div>;
+  return (
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Header />
+        <Outlet />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
