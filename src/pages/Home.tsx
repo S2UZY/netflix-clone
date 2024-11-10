51659907f5dedbb9dffa78651196f340dbc4b3eb
S2UZY <pc_suzi@daum.net>
2024-11-11 03:26:34 +0900

    feat: add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPopularMovies, IGetMoviesResult } from "../api";
+import { Loader } from "../components";
+import { MoviesLayout } from "../layout";
+
+function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>({
+    queryKey: ["movies", "popular"],
+    queryFn: getPopularMovies,
+  });
+
+  if (isLoading || !data) return <Loader />;
+
+  return <MoviesLayout data={data} />;
+}
+export default Home;
