94b044c0eb4760e600a4cd4b3d207fa7462fbbd3
S2UZY <pc_suzi@daum.net>
2024-11-11 03:26:23 +0900

    feat: add now-playing page

---
+++ src/pages/NowPlaying.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { getNowPlayingMovies, IGetMoviesResult } from "../api";
+import { Loader } from "../components";
+import { MoviesLayout } from "../layout";
+
+function NowPlaying() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>({
+    queryKey: ["movies", "now-playing"],
+    queryFn: getNowPlayingMovies,
+  });
+
+  if (isLoading || !data) return <Loader />;
+
+  return <MoviesLayout data={data} />;
+}
+export default NowPlaying;
