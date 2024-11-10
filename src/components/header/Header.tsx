cfd74e3361ee4a539a3331fde33f6df065ae30a1
S2UZY <pc_suzi@daum.net>
2024-11-11 03:28:33 +0900

    feat: add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,49 @@
+import { Link, useMatch } from "react-router-dom";
+import { useAnimation, useMotionValueEvent, useScroll } from "framer-motion";
+import { navVariants } from "./Header.animation";
+import { Circle, Col, Item, Items, Nav } from "./Header.style";
+import { Logo } from "./Logo";
+
+export function Header() {
+  const popularMatch = useMatch("/");
+  const comingSoonMatch = useMatch("/coming-soon");
+  const nowPlaying = useMatch("/now-playing");
+
+  const navAnimation = useAnimation();
+  const { scrollYProgress } = useScroll();
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    if (latest > 0.2) {
+      navAnimation.start("scroll");
+    } else {
+      navAnimation.start("top");
+    }
+
+    console.log("latest", latest);
+  });
+
+  return (
+    <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>
+            <Link to="/">
+              POPULAR {popularMatch && <Circle layoutId="circle" />}
+            </Link>
+          </Item>
+          <Item>
+            <Link to="/coming-soon">
+              COMING SOON {comingSoonMatch && <Circle layoutId="circle" />}
+            </Link>
+          </Item>
+          <Item>
+            <Link to="/now-playing">
+              NOW PLAYING {nowPlaying && <Circle layoutId="circle" />}
+            </Link>
+          </Item>
+        </Items>
+      </Col>
+    </Nav>
+  );
+}
