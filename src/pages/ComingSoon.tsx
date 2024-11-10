7fffcd2d5f16a425e653a8fb2a5a7bf0687999b0
S2UZY <pc_suzi@daum.net>
2024-11-11 03:26:53 +0900

    feat: add coming-soon page

---
+++ src/pages/ComingSoon.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { getComingSoonMovies, IGetMoviesResult } from "../api";
+import { Loader } from "../components";
+import { MoviesLayout } from "../layout";
+
+function ComingSoon() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>({
+    queryKey: ["movies", "coming-soon"],
+    queryFn: getComingSoonMovies,
+  });
+
+  if (isLoading || !data) return <Loader />;
+
+  return <MoviesLayout data={data} />;
+}
+export default ComingSoon;
